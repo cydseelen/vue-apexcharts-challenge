8dc895b3968db7ca34c23485365231fb27a1dacd
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-07-23 16:31:40 +0200

    Donut chart functioning.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div id="chart">
+        <apexchart type="donut" height="800px" :options="chartOptions" :series="series"></apexchart>
+        <h1>Cydnie</h1>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import jsonFile from './data.json';
+ export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function() {
+    return {
+    json: jsonFile,
+    series: [
+        44, 55, 41, 17, 15
+        ],
+      chartOptions: {
+        labels: ['one', 'two', 'three'],
+        chart: {
+        id: 'vuechart-example',
+        type: 'donut',
+      },
+      },
+      plotOptions: {
+        pie: {
+          size: '100%',
+        },
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      },
+      title: {
+        text: 'Monthly Stock Pricing',
+        align: 'center',
+        style: {
+          fontSize:  '20px',
+        }
+      },
+        colors: ['#00897b', 'red', 'blue'],
+    }
+    },
+      methods:{
+        init(){
+
+        },
+      },
+      mounted: function() {
+          this.init();
+        }
   }
-}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+div.chart-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center
 }
 </style>
